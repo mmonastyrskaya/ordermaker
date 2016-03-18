da5a714f56255b6b828899d4f555785b842418a3
mmonastyrskaya <monasmaria@gmail.com>
2016-03-18 17:55:04 +0300

    BludoAdded

---
+++ WpfApplication4/Bludo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProject.Entities
+{
+    class Bludo
+    {
+        [Key, Column(Order = 0)]
+        public int BludoID { get; set; }
+        [Required]
+        public string BludoName { get; set; }
+        [Required]
+        public double BludoWeight { get; set; }
+        [Required]
+        public decimal BludoPrice { get; set; }
+        [Required]
+        public string BludoCategory { get; set; }
+        public DateTime BludoTime { get; set; }
+
+        public virtual ICollection<BludoInOrder> BludoInOrders { get; set; }
+    }
+}
