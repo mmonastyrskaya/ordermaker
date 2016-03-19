ec7ce1e114d5f483a55185339e1f4012d11836db
mmonastyrskaya <monasmaria@gmail.com>
2016-03-20 02:19:22 +0300

    NewMethods

--- WpfApplication4/MainWindow.xaml.cs
+++ WpfApplication4/MainWindow.xaml.cs
@@ -26,12 +26,26 @@ namespace WpfApplication4
             InitializeComponent();
             Context cont = new Context();
             Entities.Table table = new Entities.Table();
+            Entities.Bludo bludo = new Entities.Bludo();
+
+
             table.TableID = 11;
             table.TableLabel = 100;
             table.TableLocation = "test";
             table.TablePlaces = 4;
+
+
+            bludo.BludoID=1;
+            bludo.BludoCategory = "Soup";
+            bludo.BludoName = "Gaspacho";
+            bludo.BludoPrice = 10.0m;
+            bludo.BludoWeight = 300.0;
+            bludo.BludoTime = DateTime.Parse("1900-01-01 00:00:00");
+
             //bool check = Requests.MethodsAuth.Check(new Context(),"ivanov", "1234");
-            Requests.MethodsAuth.Insert(cont, table);
+            Requests.MethodsAdmin.InsertTable(cont, table);
+            Requests.MethodsAdmin.InsertBludo(cont, bludo);
+
             //cont.SubmitChanges();
             cont.SaveChanges();
         }
