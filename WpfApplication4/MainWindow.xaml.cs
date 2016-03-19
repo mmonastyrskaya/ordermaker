9b00d6eb1dec93364f1122d49b6fc7103d54f080
mmonastyrskaya <monasmaria@gmail.com>
2016-03-20 01:52:14 +0300

    NewMethods

--- WpfApplication4/MainWindow.xaml.cs
+++ WpfApplication4/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.Linq;
 
 namespace WpfApplication4
 {
@@ -23,7 +24,16 @@ namespace WpfApplication4
         public MainWindow()
         {
             InitializeComponent();
+            Context cont = new Context();
+            Entities.Table table = new Entities.Table();
+            table.TableID = 11;
+            table.TableLabel = 100;
+            table.TableLocation = "test";
+            table.TablePlaces = 4;
             //bool check = Requests.MethodsAuth.Check(new Context(),"ivanov", "1234");
+            Requests.MethodsAuth.Insert(cont, table);
+            //cont.SubmitChanges();
+            cont.SaveChanges();
         }
     }
 }
--- WpfApplication4/Requests/MethodsAuth.cs
+++ WpfApplication4/Requests/MethodsAuth.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Data.Linq;
 
 namespace WpfApplication4.Requests
 {
@@ -27,17 +29,34 @@ namespace WpfApplication4.Requests
                 //string error = "Проверьте правильность введенного логина.";
             }
 
-            return check;
- 
-            
-            //foreach (var w in context.Waiters.Where(w => w.Waiter == supplier))
-            //{
-            //    var sID = s.SupplierID;
-            //    foreach (var o in context.Offers.Where(o => o.SupplierID == sID))
-            //    {
-            //        o.Price *= percent;
-            //    }
-            //}
+            return check;  
         }
+
+        public static void InsertTable(Context context, Entities.Table table)
+        {            
+            context.Tables.Add(table);
+            context.SaveChanges();
+        }
+
+        public static void InsertWaiter(Context context, Entities.Waiter waiter)
+        {
+            context.Waiters.Add(waiter);
+            context.SaveChanges();
+        }
+
+        public static void InsertBludo(Context context, Entities.Bludo bludo)
+        {
+            context.Bludos.Add(bludo);
+            context.SaveChanges();
+        }
+
+        //foreach (var w in context.Waiters.Where(w => w.Waiter == supplier))
+        //{
+        //    var sID = s.SupplierID;
+        //    foreach (var o in context.Offers.Where(o => o.SupplierID == sID))
+        //    {
+        //        o.Price *= percent;
+        //    }
+        //}
     }
 }
