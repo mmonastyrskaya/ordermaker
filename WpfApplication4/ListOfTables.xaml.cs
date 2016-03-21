6178ef8f40835934e15fb6b2413c586739f5a26c
palanova <palanova-lera@mail.ru>
2016-03-21 15:44:23 +0300

    AddingTable

--- WpfApplication4/ListOfTables.xaml.cs
+++ WpfApplication4/ListOfTables.xaml.cs
@@ -19,9 +19,30 @@ namespace WpfApplication4
     /// </summary>
     public partial class ListOfTables : Window
     {
+        Context context = new Context();
+
         public ListOfTables()
         {
             InitializeComponent();
+            List<Entities.Table> tables = Requests.MethodsAdmininstrator.GetAllTabless(context);
+            foreach (Entities.Table table in tables)
+            {
+                tabels.Items.Add(table.TableLabel);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            UpdatingTables ut = new UpdatingTables(int.Parse((tabels.SelectedItem).ToString()));
+            ut.Show();
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            AddingTable at = new AddingTable();
+            at.Show();
+            this.Close();
         }
     }
 }
